ed0f849991c98396d41f29e5344030f119d1a0c6
kader009 <kadermollarasel000@gmail.com>
2023-11-17 17:17:13 +0600

    setup done

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+import cors from 'cors';
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+// console.log(process.cwd());
+
+export default app;
